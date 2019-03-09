9721a17b9be99ed77dc68609ba85f0cdd4fde821
RussBeall <russbeall@gmail.com>
2019-03-08 19:56:06 -0600

    simple prime finder

---
+++ prime.js
@@ -0,0 +1,23 @@
+function sumPrimes(num) {
+  let sum = 0;
+
+  for( let i = 2; i <= num; i++) {
+    if (isPrime(i)) {
+      sum += i;
+    }
+  }
+
+  return sum;
+}
+
+function isPrime(num){
+  for (let i = 2; i < num; i++){
+    if(num % i == 0)
+      return false;
+  }
+
+  return true;
+}
+
+console.log(sumPrimes(10)); // should return 17.
+console.log(sumPrimes(977)); // should return 73156.
